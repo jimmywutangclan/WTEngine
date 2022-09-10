338a41d7a6b4b8fdd9cf802b0e994f67ecb6dd0d
Jim Wu <31392245+jimmywutangclan@users.noreply.github.com>
2022-09-10 15:55:03 -0400

    Added shader setup

--- WTEngine/include/SDLGraphicsProgram.hpp
+++ WTEngine/include/SDLGraphicsProgram.hpp
@@ -5,6 +5,12 @@
 #include "SDL2/SDL.h"
 #include "glad/glad.h"
 
+// Standard stuff
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+
 class SDLGraphicsProgram {
 public:
     // Constructor
@@ -13,11 +19,23 @@ public:
     ~SDLGraphicsProgram();
     // Loop
     void Loop();
+    // Set up shaders
+    bool SetupShaders();
+    // Load shader from glsl file to String
+    std::string LoadShader(std::string dir);
 private:
     unsigned int width;
     unsigned int height;
     SDL_Window* window;
     SDL_GLContext glContext;
+
+
+    // Temporary for base triangle
+    std::vector<float> vertexArray;
+
+    unsigned int VBO;
+    unsigned int VAO;
+    unsigned int EBO;
 };
 
 
--- WTEngine/src/SDLGraphicsProgram.cpp
+++ WTEngine/src/SDLGraphicsProgram.cpp
@@ -1,5 +1,4 @@
 #include "SDLGraphicsProgram.hpp"
-#include <iostream>
 
 // Constructor
 SDLGraphicsProgram::SDLGraphicsProgram(int w, int h) {
@@ -27,7 +26,7 @@ SDLGraphicsProgram::SDLGraphicsProgram(int w, int h) {
 		exit(EXIT_FAILURE);
 	}
 
-	// Create OpenGL Context within our window
+	// Create OpenGL Graphics Context within our window
 	glContext = SDL_GL_CreateContext(window);
 	if (glContext == NULL) {
 		std::cout << "OpenGL Context error" << std::endl;
@@ -39,6 +38,21 @@ SDLGraphicsProgram::SDLGraphicsProgram(int w, int h) {
 		std::cout << "GLAD initialization error" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+
+	// Set up OpenGL Shaders
+	if (!SetupShaders()) {
+		std::cout << "Shaders error" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	std::cout << "Setup success" << std::endl;
+
+	// Set up base vertex array
+	vertexArray = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
 
 }
 
@@ -74,7 +88,121 @@ void SDLGraphicsProgram::Loop() {
 				}
 			}
 		}
-		//Update screen of our specified window
+
+		glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Update screen of our specified window
 		SDL_GL_SwapWindow(window);
 	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OpenGL Setup logic:
+
+bool SDLGraphicsProgram::SetupShaders() {
+	std::string vertexShaderCode = LoadShader("./shaders/vert.glsl");
+	std::string fragmentShaderCode = LoadShader("./shaders/frag.glsl");
+
+	const char* vertexShaderCstr = vertexShaderCode.c_str();
+	const char* fragmentShaderCstr = fragmentShaderCode.c_str();
+
+	// Compile and bind vertex shader first
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderCstr, NULL);
+	glCompileShader(vertexShader);
+
+	// Compile and bind fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderCstr, NULL);
+	glCompileShader(fragmentShader);
+
+	// Check if shaders compiled properly
+	int vertexResult;
+	int fragResult;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexResult);
+	if (vertexResult == GL_FALSE) {
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Vertex shader compilation error" << std::endl;
+		std::cout << infoLog << std::endl;
+		glDeleteShader(vertexShader);
+	}
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragResult);
+	if (fragResult == GL_FALSE) {
+		char infoLog[512];
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Fragment shader compilation error" << std::endl;
+		std::cout << infoLog << std::endl;
+		glDeleteShader(fragmentShader);
+	}
+
+	// If compilation failed, early exit
+	if (vertexResult == GL_FALSE || fragResult == GL_FALSE) {
+		return false;
+	}
+
+	// Create a shader program and link shaders to it
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int programResult;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &programResult);
+	if (programResult == GL_FALSE) {
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Shader program linking error" << std::endl;
+		std::cout << infoLog << std::endl;
+		glDeleteProgram(shaderProgram);
+	}
+
+	// If program failed to link, early exit
+	if (programResult == GL_FALSE) {
+		return false;
+	}
+	
+	// Clean up and delete shaders, no longer needed
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	return true;
+
+}
+
+std::string SDLGraphicsProgram::LoadShader(std::string dir) {
+	std::string result;
+	std::string line;
+	std::ifstream curr(dir.c_str());
+
+	if (curr.is_open()) {
+		while (getline(curr, line)) {
+			result += line + "\n";
+		}
+	}
+
+	curr.close();
+	return result;
 }
